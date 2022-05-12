1d7457a50d3968384c3486629c062310a5977867
ocean kwak <dev@bizcowork.co.kr>
2022-05-12 19:25:00 +0900

    최초 커밋

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      'main-light': '#AECE00',
+      'main-dark': '#009249',
+      'text-highlight': '#00582A',
+    },
+    fontFamily: {
+      noto: ['"Noto Sans KR"', 'sans-serif'],
+      jua: ['Jua', 'sans-serif'],
+      dohyeon: ['"Do Hyeon"', 'sans-serif'],
+    },
+  },
+  plugins: [],
+};
